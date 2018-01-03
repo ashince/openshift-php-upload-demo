0cbc1f838e96453b57012903603929e109ea185d
ashince <ashincekd@gmail.com>
2018-01-03 14:34:12 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
         <footer>
           <div class="logo"><a href="https://www.openshift.com/"></a></div>
+          <div class="logo"><a href="http://10.10.155.40"></a></div>      
         </footer>
 </section>
 
